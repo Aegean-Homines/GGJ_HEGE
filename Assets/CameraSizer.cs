a1b27b2d1bddd77c1e9c6702b3ebad00cba6cf73
Halil Ibrahim Kayim <kayimhalil@hotmail.com>
2015-03-01 23:06:23 +0200

    support for all resolutions

---
+++ Assets/CameraSizer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSizer : MonoBehaviour {
+
+    void Awake()
+    {
+        float _windowAspect = Camera.main.aspect;
+        Camera.main.orthographicSize = (30.0f / _windowAspect) / 2f;
+    }
+}
